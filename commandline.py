24f44df64e3fc9e6d84abbcaaebb858b92764613
gloriaodipo <gloriaodipo@gmail.com>
2018-07-30 12:13:05 +0300

    [FEATURE] Add login

---
+++ commandline.py
@@ -0,0 +1,8 @@
+data  = {}
+Action = 2
+
+if Action == 2:
+    username = input ("Enter username: ")
+    password = input ("Enter password: ")
+    if username==data['username'] and password==data['password']:
+        print ('Successfully logged in')
